415509126e7f69eb0a030f2c07fa173d20e3b018
himajin624 <namab0y624@gmail.com>
2025-10-20 15:59:27 +0900

    最初のコミット

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-blue-500"> Hello tailwind</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
